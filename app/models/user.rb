5f0db68f9a80ef7f30dc4dad8d2c5be86802e3be
EC2 Default User <ec2-user@ip-172-31-37-97.us-east-2.compute.internal>
2019-06-11 09:26:29 +0000

    kakunin test commit

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+
+  validates :name, presence: true, length: { maximum: 15 }
+  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+
+  
+  has_secure_password
+  
+  has_many :topics
+  has_many :favorites
+  has_many :favorite_topics, through: :favorites, source: "topic"
+  has_many :comments
+  
+  validates :password, length: { in: 8..32 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/ }
+  validates :password_confirmation, length: { in: 8..32 }
+end
